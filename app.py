bb5c4a9139933cafa7848d1b5142282547f8f096
Hammad <hammad@Hammads-MacBook-Air.local>
2023-08-23 00:32:31 +0500

    House price prediciton model working

--- app.py
+++ app.py
@@ -1,16 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import joblib
 import numpy as np
 
 app = FastAPI()
 
+# Load the pre-trained model during startup
 @app.on_event("startup")
 def load_model():
     global model
     model = joblib.load("model.pkl")
 
+# Prediction route
 @app.post("/predict/")
-def predict(data: dict):
-    features = np.array(data["features"]).reshape(1, -1)
-    prediction = model.predict(features)
-    return {"prediction": prediction.tolist()}
+def predict(features: dict):
+    # Validate input features
+    required_features = ["LotArea", "MasVnrArea", "BsmtUnfSF", "TotalBsmtSF", "1stFlrSF", "2ndFlrSF", "GrLivArea", "GarageArea", "WoodDeckSF", "OpenPorchSF"]
+    for feature in required_features:
+        if feature not in features:
+            raise HTTPException(status_code=400, detail=f"'{feature}' is required")
+
+    # Prepare input data for prediction
+    input_data = np.array([
+        features["LotArea"],
+        features["MasVnrArea"],
+        features["BsmtUnfSF"],
+        features["TotalBsmtSF"],
+        features["1stFlrSF"],
+        features["2ndFlrSF"],
+        features["GrLivArea"],
+        features["GarageArea"],
+        features["WoodDeckSF"],
+        features["OpenPorchSF"]
+    ]).reshape(1, -1)  # Convert to a 2D array
+
+    # Make prediction using the loaded model
+    predicted_price = model.predict(input_data)[0]
+
+    return {"predicted_price": predicted_price}
